40283c9f2909e518e242235dc8c27f8f9dd62e02
Jeffrey Lee Li <Jeffrey.Lee.Li@gmail.com>
2014-07-03 11:14:59 +0800

    use uint64 for maximum range

--- _code/projecteuler/001/main.go
+++ _code/projecteuler/001/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	var (
-		n = flag.Int("n", 1000, "Upper limit: not included")
+		n = flag.Uint64("n", 1000, "Upper limit: not included")
 	)
 
 	flag.Parse()
@@ -15,20 +15,26 @@ func main() {
 	fmt.Println(multipleSumBelow(*n))
 }
 
-func multipleSumBelow(n int) int {
-	s3 := seriesSumOf(3, 3, largestMultipleBelow(3, n))
-	s5 := seriesSumOf(5, 5, largestMultipleBelow(5, n))
-	s15 := seriesSumOf(15, 15, largestMultipleBelow(15, n))
+func multipleSumBelow(n uint64) uint64 {
+	var s3, s5, s15 uint64
+
+	s3 = seriesSumOf(3, 3, largestMultipleBelow(3, n))
+	s5 = seriesSumOf(5, 5, largestMultipleBelow(5, n))
+	s15 = seriesSumOf(15, 15, largestMultipleBelow(15, n))
 
 	return s3 + s5 - s15
 }
 
-func largestMultipleBelow(x, n int) int {
-	t := n - 1 // Avoid including upper limit
+func largestMultipleBelow(x, n uint64) uint64 {
+	var t uint64
+	t = n - 1 // Avoid including upper limit
+
 	return t - t%x
 }
 
-func seriesSumOf(first, diff, last int) int {
-	num := (last-first)/diff + 1
+func seriesSumOf(first, diff, last uint64) uint64 {
+	var num uint64
+	num = (last-first)/diff + 1
+
 	return num * (first + last) / 2
 }
