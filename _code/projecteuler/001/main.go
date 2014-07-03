b99cdc162ee2cd96079f1a6cd0b8ec198c86f9e0
Jeffrey Lee Li <Jeffrey.Lee.Li@gmail.com>
2014-07-03 09:47:54 +0800

    extract a func for summing up multiples

--- _code/projecteuler/001/main.go
+++ _code/projecteuler/001/main.go
@@ -3,13 +3,19 @@ package main
 import "fmt"
 
 func main() {
+	s := multipleSumBelow(1000)
+
+	fmt.Println(s)
+}
+
+func multipleSumBelow(n int) int {
 	s := 0
 
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < n; i++ {
 		if i%3 == 0 || i%5 == 0 {
 			s += i
 		}
 	}
 
-	fmt.Println(s)
+	return s
 }
